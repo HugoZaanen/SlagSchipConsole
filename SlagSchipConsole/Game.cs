99c506f4c7bbba3a588536589027097b58c98442
Huego <hugozaanen@hotmail.com>
2018-11-19 14:27:50 +0100

    Slagschip Console

---
+++ SlagSchipConsole/Game.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SlagSchipConsole
+{
+    internal class Game
+    {
+        List<string> pos = new List<string>();
+        
+        public static void PlayBattleShip()
+        {
+            SetGame();
+        }
+
+        public static void SetGame()
+        {
+           
+        }
+    }
+}
