026c7c54451677a2e271028b8aafc02131d7912e
Huego <hugozaanen@hotmail.com>
2018-11-26 12:40:27 +0100

    Slagschip

--- SlagSchipConsole/Game.cs
+++ SlagSchipConsole/Game.cs
@@ -1,19 +1,163 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SlagSchipConsole
 {
-    internal class Game
+    public class Game
     {
-        List<string> pos = new List<string>();
-        
+        public static int[,] pos;
+
+
         public static void PlayBattleShip()
-        {
-            SetGame();
+        {   
+            pos = initializeArray();
+            SetPos(3,3, "r");
+            SetPos(0,4,"d");
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(pos[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+
+            //loops(9,4,"r");
+            
         }
 
-        public static void SetGame()
+        public static int[,] initializeArray()
         {
-           
+            int[,] positions = new int[10, 10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    positions[i, j] = 0;
+                }
+            }
+
+            return positions;
+        }
+
+        public static void SetPos(int k, int l, string m)
+        {
+            if (loops(k,l,m))
+            {
+                if(m == "r")
+                {
+                    for (int i = 0;i < 5;i++)
+                    {
+                        pos[k, l + i] = 1;
+                    }
+                }
+                else
+                {
+                    for (int i = 0;i < 5;i++)
+                    {
+                        pos[k + i, l] = 1;
+                    }
+                }
+            }
+        }
+
+        public static bool loops(int k, int l, string m)
+        {
+            if (l > 0 && l < 9 && m == "d")
+            {
+                if (!(5 + k >= 10))
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (pos[i, l - 1] == 1)
+                        {
+                            return false;
+                        }
+
+                        if (pos[i, l + 1] == 1)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (l < 1 && m == "d")
+            {
+                if (!(5 + k >= 10))
+                {
+                    for (int i = 0; i < 5; i++)
+                    {                       
+                        if (pos[i, l + 1] == 1)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (!(5 + k >= 10))
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (pos[k + i, l - 1] == 1)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (k > 0 && k < 9 && m == "r")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (pos[k + 1,l + i] == 1)
+                    {
+                        return false;
+                    }
+
+                    if (pos[k - 1, l + i] == 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if (k < 1 && m == "r")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (pos[k + 1, l + i] == 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if(m == "r")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (pos[k - 1,l + i] == 1)
+                    {
+                        return false;
+                    }                    
+                }
+            }
+            return true;
         }
     }
 }
--- SlagSchipConsole/Program.cs
+++ SlagSchipConsole/Program.cs
@@ -11,6 +11,7 @@ namespace SlagSchipConsole
         static void Main(string[] args)
         {
             Game.PlayBattleShip();
+            Console.ReadLine();
         }
     }
 }
